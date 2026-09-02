d78867d0923483d1e9f444203a0967f9a75dba59
Leandro Tamarindo <tamarindodiasl@gmail.com>
2026-09-02 03:38:35 -0300

    Added alternative caesar cipher
    Added a method of caesar cipher that adds and subtract the encryption key in alternated characters. For it to work I had to make the switch  to choose from the two encryption methods I got.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int i, encpr_key;
-char buffer[20], pswd[20];
+char buffer[20], pswd[20], option;
 
 void caesar(char *p, int n){
     for(i = 0; i < (int)strlen(p); i++){
@@ -14,15 +14,39 @@ void caesar(char *p, int n){
     }
 }
 
+void caesar_alt(char *p, int n){
+    for(i = 0; i < (int)strlen(p); i++){
+        // Disconsider the non printable charachters
+        if(p[i] < 32) return;
+        if(i % 2) p[i] += n;
+        else p[i] -= n;
+    }
+}
 
 int main(){
     printf("Password: ");
     fgets(buffer, 20, stdin);
     strcpy(pswd, buffer);
 
-    printf("Key: ");
-    scanf("%d", &encpr_key);
+    printf("Choose an option below: \n"
+            "\tA: Caesar cipher\n"
+            "\tB: Alternative Caesar cipher\n");
+    fgets(buffer, 20, stdin);
+    option = buffer[0];
 
-    caesar(pswd, encpr_key);
+    switch(option){
+        case 'A':
+            printf("Key: ");
+            scanf("%d", &encpr_key);
+            caesar(pswd, encpr_key);
+            break;
+        case 'B':
+            printf("Key: ");
+            scanf("%d", &encpr_key);
+            caesar_alt(pswd, encpr_key);
+            break;
+        default: printf("Option not found!\n");      
+    }
+
     printf("%s", pswd);
 }
