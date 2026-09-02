01e130b0ed814130ed7072d40b7d3d0a79c5dfc0
Leandro Tamarindo <tamarindodiasl@gmail.com>
2026-09-02 03:21:01 -0300

    Caesar cipher
    Added the caesar cipher and the input of the encryption key for it.

--- main.c
+++ main.c
@@ -3,11 +3,26 @@
 #include <math.h>
 #include <string.h>
 
+int i, encpr_key;
 char buffer[20], pswd[20];
+
+void caesar(char *p, int n){
+    for(i = 0; i < (int)strlen(p); i++){
+        // Disconsider the non printable charachters
+        if(p[i] < 32) return;
+        p[i] += n;
+    }
+}
+
 
 int main(){
     printf("Password: ");
     fgets(buffer, 20, stdin);
     strcpy(pswd, buffer);
+
+    printf("Key: ");
+    scanf("%d", &encpr_key);
+
+    caesar(pswd, encpr_key);
     printf("%s", pswd);
 }
